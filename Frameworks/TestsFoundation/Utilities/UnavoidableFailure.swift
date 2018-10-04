f37f2140bb307e8d7f0d4bba9816461dde24bc7c
Artyom Y. Razinov <arazinov@avito.ru>
2018-10-04 21:58:18 +0300

    Add unavoidable failure

---
+++ Frameworks/TestsFoundation/Utilities/UnavoidableFailure.swift
@@ -0,0 +1,25 @@
+import Foundation
+import XCTest
+
+// Stops current test with failure, but does not affect other tests.
+public final class UnavoidableFailure {
+    public static func fail(
+        _ message: String = "",
+        testCase: XCTestCase? = nil,
+        file: StaticString = #file,
+        line: UInt = #line)
+        -> Never
+    {
+        testCase?.continueAfterFailure = false
+        XCTFail(message, file: file, line: line)
+        NSException(name: UnavoidableFailureException, reason: message).raise()
+        
+        // To produce `Never` return value. Note that it will not be executed after
+        // raising an exception on the previous line
+        fatalError(message)
+    }
+    
+    static let UnavoidableFailureException = NSExceptionName(rawValue: "UnavoidableFailureException")
+    
+    private init() {}
+}
