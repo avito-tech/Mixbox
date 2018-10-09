5725451201fe0a7a1a6321128643c6dbb8cf7843
Artyom Y. Razinov <arazinov@avito.ru>
2018-10-09 22:02:08 +0300

    Support destinations in logic_tests.sh

--- ci/logic_tests.sh
+++ ci/logic_tests.sh
@@ -3,16 +3,39 @@
 set -ex
 set -o pipefail
 
+tmpDir=/tmp/EAF744B8-3A57-473C-95E9-B1A7B7FA1BDD
+scriptDir=$(cd "$(dirname $0)"; pwd)
+
+rm -rf "$tmpDir"
+mkdir -p "$tmpDir"
+derivedDataPath=$tmpDir/dd
+
 cd `dirname $0`/../Tests
 
 cmp Podfile.lock Pods/Manifest.lock || pod install
 
 which xcpretty || brew install xcpretty
+which jq || brew install jq
+
+destination() {
+    cat "$(destinationFile)"
+}
+
+destinationFile() {
+    echo "$scriptDir/destinations/$MIXBOX_CI_DESTINATION"
+}
+
+xcodeDestination() {
+    local name=`destination | jq -r .[0].testDestination.deviceType`
+    local os=`destination | jq -r .[0].testDestination.iOSVersion`
+    echo "platform=iOS Simulator,name=$name,OS=$os"
+}
 
 xcodebuild \
 test \
 -workspace Tests.xcworkspace \
 -scheme UnitTests \
 -sdk iphonesimulator \
--destination 'platform=iOS Simulator,name=iPhone 7,OS=11.4' \
+-derivedDataPath "$derivedDataPath" \
+-destination "$(xcodeDestination)" \
 | xcpretty -r junit -o "$MIXBOX_CI_REPORTS_PATH/junit.xml"
