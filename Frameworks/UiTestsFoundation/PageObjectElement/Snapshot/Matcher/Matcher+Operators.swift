ac8360e56093f44a9da696d34872647214ab9dae
Artyom Y. Razinov <arazinov@avito.ru>
2019-04-09 23:41:38 +0300

    Add TODO

--- Frameworks/UiTestsFoundation/PageObjectElement/Snapshot/Matcher/Matcher+Operators.swift
+++ Frameworks/UiTestsFoundation/PageObjectElement/Snapshot/Matcher/Matcher+Operators.swift
@@ -1,3 +1,5 @@
+// TODO: Should we convert AndMatcher([a, AndMatcher([b, c])]) to AndMatcher([a, b, c])?
+//       Or should we at least make logs less like a hell if there are a lot of matchers combined with && or ||?
 public extension Matcher {
     static func &&(left: Matcher, right: Matcher) -> Matcher {
         return AndMatcher([left, right])
