7837e762ac5be03d2502c8513cf9d25237d386bb
Artyom Y. Razinov <arazinov@avito.ru>
2019-05-05 17:45:58 +0300

    Split long function to smaller functions
    Function was 77 lines long. This helped to find a bug. TODO was added.

--- Frameworks/UiTestsFoundation/PageObjectElement/Finder/RealHierarchy/RealViewHierarchyElementQuery.swift
+++ Frameworks/UiTestsFoundation/PageObjectElement/Finder/RealHierarchy/RealViewHierarchyElementQuery.swift
@@ -31,80 +31,86 @@ final class RealViewHierarchyElementQuery: ElementQuery {
     func resolveElement(interactionMode: InteractionMode) -> ResolvedElementQuery {
         let stepLogBefore = StepLogBefore.other("Поиск элемента")
         
-        // TODO: Extract code to functions
-        let wrapper = stepLogger.logStep(stepLogBefore: stepLogBefore) { () -> StepLoggerResultWrapper<ResolvedElementQuery> in
-            let elementWasFound: Bool
-            
-            let result = ipcClient.call(
-                method: ViewHierarchyIpcMethod()
-            )
-            
-            let elementQueryResolvingState = ElementQueryResolvingState()
-            
-            var artifacts = [Artifact]()
-            let resolvedElementQuery: ResolvedElementQuery
-            
-            if let viewHierarchy = result.data {
-                // We don't actually need start/stop. TODO!
-                elementQueryResolvingState.start()
-                
-                for element in viewHierarchy.rootElements {
-                    matchRecursively(element: element, state: elementQueryResolvingState)
-                }
-                
-                elementQueryResolvingState.stop()
-                
-                resolvedElementQuery = ResolvedElementQuery(elementQueryResolvingState: elementQueryResolvingState)
-                
-                elementWasFound = resolvedElementQuery.matchingSnapshots.count > 0
-                
-                if let failureDescription = resolvedElementQuery.candidatesDescription() {
-                    artifacts.append(
-                        Artifact(
-                            name: "Кандидаты",
-                            content: .text(failureDescription)
-                        )
-                    )
-                    artifacts.append(
-                        Artifact(
-                            name: "Иерархия вьюх",
-                            content: .text(
-                                viewHierarchy.debugDescription
-                            )
-                        )
-                    )
-                    if let screenshot = screenshotTaker.takeScreenshot() {
-                        artifacts.append(
-                            Artifact(
-                                name: "Скриншот",
-                                content: .screenshot(screenshot)
-                            )
-                        )
-                    }
-                }
-            } else {
-                resolvedElementQuery = ResolvedElementQuery(elementQueryResolvingState: elementQueryResolvingState)
-                
-                elementWasFound = false
-                
-                // TODO: better FileLine
-                testFailureRecorder.recordFailure(
-                    description: "Не удалось получить иерархию вьюх из приложения",
-                    fileLine: FileLine.current(),
-                    shouldContinueTest: false
-                )
-            }
-            
-            return StepLoggerResultWrapper(
-                stepLogAfter: StepLogAfter(
-                    wasSuccessful: elementWasFound,
-                    artifacts: artifacts
-                ),
-                wrappedResult: resolvedElementQuery
-            )
+        let wrapper = stepLogger.logStep(stepLogBefore: stepLogBefore) {
+            resolveElementWhileBeingLogged(interactionMode: interactionMode)
         }
         
         return wrapper.wrappedResult
+    }
+    
+    // TODO: interactionMode is unused. This will produce bugs. See XcuiElementQuery for reference.
+    // TODO: Write test. There is no test for that. E.g.: trying to get element #2 from matching element should
+    //       produce proper failure if there is only 1 matching element. Maybe there should be also some other logic.
+    private func resolveElementWhileBeingLogged(
+        interactionMode: InteractionMode)
+        -> StepLoggerResultWrapper<ResolvedElementQuery>
+    {
+        switch viewHierarchy() {
+        case .data(let viewHierarchy):
+            return getResolvedElementQueryWhileBeingLogged(viewHierarchy: viewHierarchy)
+        case .error(let error):
+            return reportIpcFailedWhileGettingHierarchy(error: error)
+        }
+    }
+    
+    private func viewHierarchy()
+        -> DataResult<ViewHierarchy, IpcClientError>
+    {
+        return ipcClient.call(
+            method: ViewHierarchyIpcMethod()
+        )
+    }
+    
+    private func reportIpcFailedWhileGettingHierarchy(
+        error: IpcClientError)
+        -> StepLoggerResultWrapper<ResolvedElementQuery>
+    {
+        // TODO: better FileLine (should point to invocation in test)
+        testFailureRecorder.recordFailure(
+            description: "Не удалось получить иерархию вьюх из приложения",
+            fileLine: FileLine.current(),
+            shouldContinueTest: false
+        )
+        
+        // TODO: Get rid of ElementQueryResolvingState, it is only a kludge for XCUI
+        let emptyState = ElementQueryResolvingState()
+        emptyState.start()
+        emptyState.stop()
+        
+        let emptyQuery = ResolvedElementQuery(
+            elementQueryResolvingState: emptyState
+        )
+        
+        return StepLoggerResultWrapper(
+            stepLogAfter: StepLogAfter(
+                wasSuccessful: false,
+                artifacts: []
+            ),
+            wrappedResult: emptyQuery
+        )
+    }
+    
+    private func getResolvedElementQueryWhileBeingLogged(viewHierarchy: ViewHierarchy)
+        -> StepLoggerResultWrapper<ResolvedElementQuery>
+    {
+        let resolvedElementQuery = resolveElementQuery(viewHierarchy: viewHierarchy)
+        
+        return log(resolvedElementQuery: resolvedElementQuery, viewHierarchy: viewHierarchy)
+    }
+    
+    private func resolveElementQuery(viewHierarchy: ViewHierarchy) -> ResolvedElementQuery {
+        let elementQueryResolvingState = ElementQueryResolvingState()
+        
+        // We don't actually need start/stop. It is a kludge for XCUI. TODO: Get rid of it.
+        elementQueryResolvingState.start()
+        
+        for element in viewHierarchy.rootElements {
+            matchRecursively(element: element, state: elementQueryResolvingState)
+        }
+        
+        elementQueryResolvingState.stop()
+        
+        return ResolvedElementQuery(elementQueryResolvingState: elementQueryResolvingState)
     }
     
     private func matchRecursively(
@@ -119,5 +125,54 @@ final class RealViewHierarchyElementQuery: ElementQuery {
         for child in element.children {
             matchRecursively(element: child, parent: snapshot, state: state)
         }
+    }
+    
+    private func log(resolvedElementQuery: ResolvedElementQuery, viewHierarchy: ViewHierarchy)
+        -> StepLoggerResultWrapper<ResolvedElementQuery>
+    {
+        let elementWasFound = resolvedElementQuery.matchingSnapshots.count > 0
+        
+        return StepLoggerResultWrapper(
+            stepLogAfter: StepLogAfter(
+                wasSuccessful: elementWasFound,
+                artifacts: artifactsForLog(
+                    resolvedElementQuery: resolvedElementQuery,
+                    viewHierarchy: viewHierarchy
+                )
+            ),
+            wrappedResult: resolvedElementQuery
+        )
+    }
+    
+    // TODO: This logic is copypasted from XcuiElementQuery. Share.
+    private func artifactsForLog(resolvedElementQuery: ResolvedElementQuery, viewHierarchy: ViewHierarchy) -> [Artifact] {
+        var artifacts = [Artifact]()
+        
+        if let candidatesDescription = resolvedElementQuery.candidatesDescription() {
+            artifacts.append(
+                Artifact(
+                    name: "Кандидаты",
+                    content: .text(candidatesDescription)
+                )
+            )
+            artifacts.append(
+                Artifact(
+                    name: "Иерархия вьюх",
+                    content: .text(
+                        viewHierarchy.debugDescription
+                    )
+                )
+            )
+            if let screenshot = screenshotTaker.takeScreenshot() {
+                artifacts.append(
+                    Artifact(
+                        name: "Скриншот",
+                        content: .screenshot(screenshot)
+                    )
+                )
+            }
+        }
+        
+        return artifacts
     }
 }
