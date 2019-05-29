05d04aa2de48736188a182a9c65a810161d3a188
Artyom Y. Razinov <artyom.razinov@gmail.com>
2019-05-29 05:26:46 +0300

    Fix Demo

--- Demo/MixboxDemoUITests/MixboxDemoUITests.swift
+++ Demo/MixboxDemoUITests/MixboxDemoUITests.swift
@@ -4,9 +4,9 @@ class DemoTests: TestCase {
     func test() {
         launch()
         
-        pageObjects.firstScreen.label.assert.hasText("Label")
+        pageObjects.firstScreen.label.assertHasText("Label")
         pageObjects.firstScreen.button.tap()
         
-        pageObjects.secondScreen.otherLabel.assert.hasText("Other Label")
+        pageObjects.secondScreen.otherLabel.assertHasText("Other Label")
     }
 }
--- Demo/MixboxDemoUITests/TestCase/TestCaseDependencies.swift
+++ Demo/MixboxDemoUITests/TestCase/TestCaseDependencies.swift
@@ -13,52 +13,39 @@ final class TestCaseDependencies {
         let currentTestCaseProvider = AutomaticCurrentTestCaseProvider()
         let screenshotTaker = XcuiScreenshotTaker()
         let stepLogger = XcuiActivityStepLogger(originalStepLogger: StepLoggerImpl())
-        let snapshotsComparisonUtility = SnapshotsComparisonUtilityImpl(
-            // TODO
-            basePath: "/tmp/35B74059-CF04-4679-853C-9B6C961BDAA8/UITests/Screenshots"
-        )
-        let snapshotCaches = SnapshotCachesImpl.create(cachingEnabled: false)
         let applicationProvider = ApplicationProviderImpl { XCUIApplication() }
+        let ipcClient = SbtuiIpcClient(
+            application: application
+        )
+        
+        let spinner: Spinner = SpinnerImpl(
+            runLoopSpinnerFactory: RunLoopSpinnerFactoryImpl(
+                runLoopModesStackProvider: RunLoopModesStackProviderImpl()
+            )
+        )
         
         pageObjects = PageObjects(
             pageObjectDependenciesFactory: XcuiPageObjectDependenciesFactory(
-                interactionExecutionLogger: InteractionExecutionLoggerImpl(
-                    stepLogger: stepLogger,
-                    screenshotTaker: screenshotTaker,
-                    imageHashCalculator: DHashV0ImageHashCalculator()
-                ),
                 testFailureRecorder: XcTestFailureRecorder(
                     currentTestCaseProvider: currentTestCaseProvider
                 ),
-                ipcClient: SbtuiIpcClient(
-                    application: application
-                ),
-                snapshotsComparisonUtility: snapshotsComparisonUtility,
+                ipcClient: ipcClient,
                 stepLogger: stepLogger,
                 pollingConfiguration: .reduceWorkload,
-                snapshotCaches: snapshotCaches,
-                elementFinder: XcuiElementFinder(
+                elementFinder:
+                XcuiElementFinder(
                     stepLogger: stepLogger,
-                    snapshotCaches: snapshotCaches,
-                    applicationProviderThatDropsCaches: applicationProvider
+                    applicationProviderThatDropsCaches: applicationProvider,
+                    screenshotTaker: XcuiScreenshotTaker()
                 ),
                 applicationProvider: applicationProvider,
-                applicationCoordinatesProvider: ApplicationCoordinatesProviderImpl(applicationProvider: applicationProvider),
-                eventGenerator: EventGeneratorImpl(applicationProvider: applicationProvider)
+                eventGenerator: XcuiEventGenerator(
+                    applicationProvider: applicationProvider
+                ),
+                screenshotTaker: screenshotTaker,
+                pasteboard: IpcPasteboard(ipcClient: ipcClient),
+                spinner: spinner
             )
         )
-    }
-    
-    // TODO: Fix without a kludge.
-    private func thisFunctionIsNotUsedAnywhereButEventuallyFixesCrash() {
-        // This line fixes crash:
-        //
-        // 2019-02-26 00:34:33.020685+0300 MixboxDemoUITests-Runner[61838:18544173] (dlopen_preflight(/Users/razinov/Library/Developer/Xcode/DerivedData/MixboxDemo-fpcsjryqkqkzcwdqcjtpfaotnztn/Build/Products/Debug-iphonesimulator/MixboxDemoUITests-Runner.app/PlugIns/MixboxDemoUITests.xctest/MixboxDemoUITests): Library not loaded: @rpath/libswiftSwiftOnoneSupport.dylib
-        // Referenced from: /Users/razinov/Library/Developer/Xcode/DerivedData/MixboxDemo-fpcsjryqkqkzcwdqcjtpfaotnztn/Build/Products/Debug-iphonesimulator/MixboxDemoUITests-Runner.app/PlugIns/MixboxDemoUITests.xctest/Frameworks/MixboxArtifacts.framework/MixboxArtifacts
-        // Reason: image not found)
-        //
-        // Source: https://stackoverflow.com/questions/40986082/dyld-library-not-loaded-rpath-libswiftswiftononesupport-dylib
-        //
-        print("")
     }
 }
