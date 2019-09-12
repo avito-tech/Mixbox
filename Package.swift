9d2e0892d491e914b8312fc77db4616923f8de1b
Artyom Y. Razinov <arazinov@avito.ru>
2019-09-12 14:36:27 +0300

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "Mixbox",
+    products: [
+        .library(name: "MixboxAnyCodable", targets: [ "MixboxAnyCodable" ]),
+    ],
+    targets: [
+        .target(
+            name: "MixboxAnyCodable",
+            dependencies: [],
+            path: "Frameworks/AnyCodable",
+            swiftSettings: [
+                .define("MIXBOX_ENABLE_IN_APP_SERVICES"),
+            ]
+        )
+    ]
+)
