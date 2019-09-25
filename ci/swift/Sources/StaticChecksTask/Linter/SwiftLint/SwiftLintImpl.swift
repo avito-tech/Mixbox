a9a617f651e2b3f5139fa215d9485dc68c731323
Artyom Y. Razinov <arazinov@avito.ru>
2019-09-25 15:11:10 +0300

    Improve readability of SwiftLintImpl

--- ci/swift/Sources/StaticChecksTask/Linter/SwiftLint/SwiftLintImpl.swift
+++ ci/swift/Sources/StaticChecksTask/Linter/SwiftLint/SwiftLintImpl.swift
@@ -10,6 +10,11 @@ public final class SwiftLintImpl: SwiftLint {
     private let repoRootProvider: RepoRootProvider
     private let swiftLintViolationsParser: SwiftLintViolationsParser
     
+    private struct SetUpSwiftLint {
+        let swiftlintScriptPath: String
+        let sourcesToLintPath: String
+    }
+    
     public init(
         processExecutor: ProcessExecutor,
         repoRootProvider: RepoRootProvider,
@@ -21,6 +26,16 @@ public final class SwiftLintImpl: SwiftLint {
     }
     
     public func lint() throws {
+        let setUpSwiftLint = try self.setUpSwiftLint()
+        
+        let swiftlintResult = try executeSwiftLint(
+            setUpSwiftLint: setUpSwiftLint
+        )
+        
+        try handle(swiftlintResult: swiftlintResult)
+    }
+    
+    private func setUpSwiftLint() throws -> SetUpSwiftLint {
         let repoRootPath = try repoRootProvider.repoRootPath()
         
         let testsProjectDirectory = "\(repoRootPath)/Tests"
@@ -30,29 +45,40 @@ public final class SwiftLintImpl: SwiftLint {
             currentDirectory: testsProjectDirectory
         )
         
-        let swiftlintScriptPath = "\(testsProjectDirectory)/Pods/SwiftLint/swiftlint"
-        
-        let result = try processExecutor.execute(
-            executable: swiftlintScriptPath,
+        return SetUpSwiftLint(
+            swiftlintScriptPath: "\(testsProjectDirectory)/Pods/SwiftLint/swiftlint",
+            sourcesToLintPath: repoRootPath
+        )
+    }
+    
+    private func executeSwiftLint(
+        setUpSwiftLint: SetUpSwiftLint)
+        throws
+        -> ProcessResult
+    {
+        return try processExecutor.execute(
+            executable: setUpSwiftLint.swiftlintScriptPath,
             arguments: [],
-            currentDirectory: repoRootPath,
+            currentDirectory: setUpSwiftLint.sourcesToLintPath,
             environment: [:],
             stdoutDataHandler: { _ in },
             stderrDataHandler: { _ in }
         )
-        
-        if let stdout = result.stdout.utf8String() {
+    }
+    
+    private func handle(swiftlintResult: ProcessResult) throws {
+        if let stdout = swiftlintResult.stdout.utf8String() {
             let violations = try swiftLintViolationsParser.parseViolations(stdout: stdout)
             
             if violations.isEmpty {
-                if result.code != 0 {
+                if swiftlintResult.code != 0 {
                     throw ErrorString(
                         """
-                        Linting failed with exit code \(result.code), but found 0 violations
+                        Linting failed with exit code \(swiftlintResult.code), but found 0 violations
                         """
                     )
                 } else {
-                    // ok
+                    // SwiftLint checks have passed successfully
                 }
             } else {
                 print("Found SwiftLint violations:")
@@ -66,11 +92,10 @@ public final class SwiftLintImpl: SwiftLint {
                     )
                 }
             }
-            
-        } else if result.code != 0 {
+        } else if swiftlintResult.code != 0 {
             throw ErrorString(
                 """
-                Linting failed with exit code \(result.code), failed to parse stderr
+                Linting failed with exit code \(swiftlintResult.code), failed to parse stderr
                 """
             )
         }
