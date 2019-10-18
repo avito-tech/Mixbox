cae0b2cf47265e8995d3e51a2d36f0464f4c6414
Artyom Y. Razinov <arazinov@avito.ru>
2019-10-18 18:30:34 +0300

    Set platforms to `[.macOS(.v10_14)]` in Package.swift

--- ci/swift/Package.swift
+++ ci/swift/Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "MixboxSwiftCI",
+    platforms: [
+        .macOS(.v10_14),
+    ],
     products: [
         .executable(
             name: "TeamcityGrayBoxTestsBuild",
