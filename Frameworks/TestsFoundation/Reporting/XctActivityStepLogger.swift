0e349599dbfb9330d6c9220286fc0f4bc093b8bd
Artyom Y. Razinov <arazinov@avito.ru>
2020-03-11 18:11:31 +0300

    Fix displaying test logs in Xcode

--- Frameworks/TestsFoundation/Reporting/XctActivityStepLogger.swift
+++ Frameworks/TestsFoundation/Reporting/XctActivityStepLogger.swift
@@ -20,7 +20,10 @@ public final class XctActivityStepLogger: StepLogger {
         -> StepLoggerResultWrapper<T>
     {
         return originalStepLogger.logStep(stepLogBefore: stepLogBefore) { [xctAttachmentsAdder] () -> StepLoggerResultWrapper<T> in
-            XCTContext.runActivity(named: stepLogBefore.title) { (activity: XCTActivity) -> StepLoggerResultWrapper<T> in
+            // Xcode 11.3 stopped rendering multiline log entries properly.
+            // Multiline entries are trimmed to a single line, text is centered in a fixed height cell.
+            let activityTitle = stepLogBefore.title.replacingOccurrences(of: "\n", with: " ")
+            return XCTContext.runActivity(named: activityTitle) { (activity: XCTActivity) -> StepLoggerResultWrapper<T> in
                 xctAttachmentsAdder.add(attachments: stepLogBefore.attachments, activity: activity)
                 
                 let result = body()
