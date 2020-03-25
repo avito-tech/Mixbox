787274aaf2da48e7ee091a86b6d6d43b5e3459d3
Artyom Y. Razinov <arazinov@avito.ru>
2020-03-25 19:22:52 +0300

    Add comment

--- Frameworks/UiTestsFoundation/PageObjectElement/Snapshot/Matcher/Impls/Generic/Logic/AndMatcher.swift
+++ Frameworks/UiTestsFoundation/PageObjectElement/Snapshot/Matcher/Impls/Generic/Logic/AndMatcher.swift
@@ -1,3 +1,5 @@
+// TODO: Ability to not iterate over every element.
+// This will increase performance, but make log
 public class AndMatcher<T>: CompoundMatcher<T> {
     public init(_ matchers: [Matcher<T>]) {
         super.init(
