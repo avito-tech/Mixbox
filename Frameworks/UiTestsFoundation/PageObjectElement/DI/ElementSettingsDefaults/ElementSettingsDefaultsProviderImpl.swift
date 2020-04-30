8cb7d1cb90ed364d4546320197d88f64cb358637
Artyom Y. Razinov <arazinov@avito.ru>
2020-04-30 16:18:18 +0300

    Decrease default `percentageOfVisibleArea` from 0.6 to 0.5
    This is because a lot of views can be overlapped by keyboard. 0.5 is enough.

--- Frameworks/UiTestsFoundation/PageObjectElement/DI/ElementSettingsDefaults/ElementSettingsDefaultsProviderImpl.swift
+++ Frameworks/UiTestsFoundation/PageObjectElement/DI/ElementSettingsDefaults/ElementSettingsDefaultsProviderImpl.swift
@@ -7,15 +7,27 @@ public final class ElementSettingsDefaultsProviderImpl: ElementSettingsDefaultsP
             scrollMode: .default,
             interactionTimeout: 15,
             interactionMode: .default,
-            // A lot of root views are overlapped with navbar/status bar/tabbar.
+            //
+            // `percentageOfVisibleArea` specifies how much of the element should be visible, for example, for check to pass.
+            //
+            // A lot of root views are overlapped with navbar/status bar/tabbar (such views are about 90% visible).
             //
             // The default value of 1.0 will make checks for those views fail, and it is expected
             // to have about one such view for page object in real application.
             //
+            // But when keyboard is open on smaller devices then only about 50% of view is visible. So 50% is a default
+            // value. The idea behind the default value is to minimize amount of customization needed in tests, ideally
+            // to zero, while maximizing this value to improve quality of tests (ability to find layout bugs for example).
+            // In a pretty big project 50% works well.
+            //
             // The value should ideally be greater than 0.5, because value greater than 0.5
             // indicates that the center is visible (it is true for pure vertical and pure horizontal layouts
             // and rectangle shapes of views). And the center is a default point for most gestures.
-            percentageOfVisibleArea: 0.6
+            //
+            // But the problem with tapping should be solved with other measures, like detecting actually visible
+            // pixels for tapping them instead of center of the element.
+            //
+            percentageOfVisibleArea: 0.5
         )
     }
 }
