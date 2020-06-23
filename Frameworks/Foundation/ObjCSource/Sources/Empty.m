2b7e265da93977d18092f0f4eeccb2558306e614
Eugene Antropov <e@antropov.it>
2020-06-23 16:36:39 +0300

    Fix dublicate empty class

--- Frameworks/Foundation/ObjCSource/Sources/Empty.m
+++ Frameworks/Foundation/ObjCSource/Sources/Empty.m
@@ -1,7 +1,7 @@
 #import <Foundation/Foundation.h>
 
-@interface EmptyClass : NSObject
+@interface EmptyClass_bf54cf38879b430fb36aa6fde6604e34 : NSObject
 @end
 
-@implementation EmptyClass {}
+@implementation EmptyClass_bf54cf38879b430fb36aa6fde6604e34 {}
 @end
--- Frameworks/Gray/Utils/PrivateHeaders/Empty.m
+++ Frameworks/Gray/Utils/PrivateHeaders/Empty.m
@@ -1,7 +1,7 @@
 #import <Foundation/Foundation.h>
 
-@interface EmptyClass : NSObject
+@interface EmptyClass_00e67721e742461bb8c7fa58e037d6e7 : NSObject
 @end
 
-@implementation EmptyClass {}
+@implementation EmptyClass_00e67721e742461bb8c7fa58e037d6e7 {}
 @end
