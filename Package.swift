6df63ad835fd4c9c7e1881d8f39305d465908e1b
Eugene Antropov <e@antropov.it>
2020-06-23 19:40:17 +0300

    Components disabled =(

--- Package.swift
+++ Package.swift
@@ -86,7 +86,7 @@ let package = Package(
                 cSettings: cSettings(),
                 cxxSettings: cxxSettings(),
                 swiftSettings: swiftSettings(),
-                linkerSettings: [.linkedFramework("XCTest"),.linkedLibrary("swiftXCTest"), .linkedFramework("XCTAutomationSupport"), .unsafeFlags(["-F$(PLATFORM_DIR)/Developer/Library/PrivateFrameworks"])]),
+                linkerSettings: [.linkedFramework("XCTest"),.linkedLibrary("swiftXCTest"), .linkedFramework("XCTAutomationSupport"), .unsafeFlags(["-F/Applications/XCode.app/Contents/Developer/Library/PrivateFrameworks"])]),
         
         // MARK: - MixboxBuiltinIpc
         .target(name: "MixboxBuiltinIpc_objc", dependencies: [
