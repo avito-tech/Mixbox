2a7d8242aba04d76cbac634d8eefaadb07beb124
Eugene Antropov <e@antropov.it>
2020-07-30 12:36:59 +0300

    Reset

--- Frameworks/InAppServices/Features/WaitingForQuiescence/CoreAnimation/SurrogateCAAnimationDelegate.swift
+++ Frameworks/InAppServices/Features/WaitingForQuiescence/CoreAnimation/SurrogateCAAnimationDelegate.swift
@@ -15,7 +15,7 @@ private func AnimationDidStart(
 ) {
     animation?.mb_state = .started
     
-    if !isInvokedFromSwizzledMethod {
+    if isInvokedFromSwizzledMethod {
         let selector = #selector(SurrogateCAAnimationDelegate.mbswizzled_animationDidStart(_:))
         let originalImp = class_getMethodImplementation(type(of: self), selector)
         unsafeBitCast(originalImp, to: AnimationDidStartFunction.self)(self, selector, animation)
@@ -30,7 +30,7 @@ private func AnimationDidStop(
 ) {
     animation?.mb_state = .stopped
     
-    if !isInvokedFromSwizzledMethod {
+    if isInvokedFromSwizzledMethod {
         let selector = #selector(SurrogateCAAnimationDelegate.mbswizzled_animationDidStop(_:finished:))
         let originalImp = class_getMethodImplementation(type(of: self), selector)
         unsafeBitCast(originalImp, to: AnimationDidFinishFunction.self)(self, selector, animation, finished)
