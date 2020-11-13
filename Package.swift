43ad97e9024795838e62c94326c668068aa88534
Artyom Y. Razinov <arazinov@avito.ru>
2020-11-13 17:45:00 +0300

    Add all frameworks to Package.swift

--- Package.swift
+++ Package.swift
@@ -2,19 +2,48 @@
 
 import PackageDescription
 
+let frameworksByHavingMixedSources = [
+    "AnyCodable": false,
+    "Black": true,
+    "BuiltinDi": false,
+    "BuiltinIpc": true,
+    "Di": false,
+    "FakeSettingsAppMain": false,
+    "Foundation": false,
+    "Generators": false,
+    "Gray": false,
+    "InAppServices": true,
+    "IoKit": true,
+    "Ipc": false,
+    "IpcCommon": false,
+    "IpcSbtuiClient": false,
+    "IpcSbtuiHost": false,
+    "MocksRuntime": false,
+    "Reflection": false,
+    "Stubbing": false,
+    "Testability": true,
+    "TestsFoundation": true,
+    "UiKit": false,
+    "UiTestsFoundation": false
+]
+
+let frameworks = frameworksByHavingMixedSources
+    .filter { framework, hasMixedSourced in !hasMixedSourced }
+    .map { framework, _ in framework }
+
 let package = Package(
     name: "Mixbox",
-    products: [
-        .library(name: "MixboxAnyCodable", targets: [ "MixboxAnyCodable" ])
-    ],
-    targets: [
+    products: frameworks.map {
+        .library(name: "Mixbox\($0)", targets: [ "Mixbox\($0)" ])
+    },
+    targets: frameworks.map {
         .target(
-            name: "MixboxAnyCodable",
+            name: "Mixbox\($0)",
             dependencies: [],
-            path: "Frameworks/AnyCodable",
+            path: "Frameworks/\($0)",
             swiftSettings: [
                 .define("MIXBOX_ENABLE_IN_APP_SERVICES")
             ]
         )
-    ]
+    }
 )
